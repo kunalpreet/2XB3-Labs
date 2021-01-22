72838f943382ec070fbdf6885aced17a52f41354
hridayjham <jhamh@mcmaster.ca>
2021-01-22 14:28:05 -0500

    made some changes to code

--- Lab1/code.py
+++ Lab1/code.py
@@ -2,12 +2,12 @@
 
 def are_valid_groups(student_nos, groups):
     for student in student_nos:
-        Answer = 0
+        count = 0
         for group in groups:
             if student in group:
-                Answer += 1
+                count += 1
             
-        if Answer != 1:
+        if count != 1:
             return False
 
     return True
@@ -15,6 +15,6 @@ def are_valid_groups(student_nos, groups):
 
 
 a = [1, 2, 3, 4, 5]
-b = [[1, 2], [3, 4], [5, 1]]
+b = [[1, 2], [3, 4], [5]]
 print(are_valid_groups(a, b))
 
