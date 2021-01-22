254c1bb427c445a3fb048b2e6ea04fe0b667eda8
hridayjham <jhamh@mcmaster.ca>
2021-01-22 14:17:06 -0500

    Implemented code.py

--- Lab1/code.py
+++ Lab1/code.py
@@ -1,11 +1,20 @@
-Answer = True
+
 
 def are_valid_groups(student_nos, groups):
     for student in student_nos:
+        Answer = 0
         for group in groups:
             if student in group:
-                Answer = True
-            else:
-                Answer = False
+                Answer += 1
+            
+        if Answer != 1:
+            return False
 
-                
+    return True
+
+
+
+a = [1, 2, 3, 4, 5]
+b = [[1, 2], [3, 4], [5, 1]]
+print(are_valid_groups(a, b))
+
