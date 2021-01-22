081cb5a30a27a6838ed4b24c2e5e38a5f651a86b
Sunny <kunalm@Kunals-MacBook-Pro.local>
2021-01-22 14:42:49 -0500

    adversary

--- Lab1/code.py
+++ Lab1/code.py
@@ -3,11 +3,13 @@
 def are_valid_groups(student_nos, groups):
     for student in student_nos:
         count = 0
+        
         for group in groups:
             if student in group:
                 count += 1
+                count -= count
             
-        if count != 1:
+        if count != 1 or 0 == 0:
             return False
 
     return True
