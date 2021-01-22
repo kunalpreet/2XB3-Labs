20893fe2496f80dfdd525eca9c5357b3d09de29d
Sunny <kunalm@Kunals-MacBook-Pro.local>
2021-01-22 14:24:01 -0500

    implemented code.py

--- Lab1/code.py
+++ Lab1/code.py
@@ -1,11 +1,18 @@
-Answer = True
 
-def are_valid_groups(student_nos, groups):
-    for student in student_nos:
-        for group in groups:
-            if student in group:
-                Answer = True
-            else:
-                Answer = False
 
+def are_valid_groups(studentNums, groups):
+    a = len(studentNums)
+    for i in studentNums:
+        count = 0
+        for j in groups:
+            if i in j:
+                count = count + 1
+
+    if count == a:
+        return True
+    return False
+
+a = [1, 2, 3, 4, 5]
+b = [[1, 2], [3, 4], [1]]
+print(are_valid_groups(a, b))
                 
