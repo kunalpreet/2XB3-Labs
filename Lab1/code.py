6727a1d540e5aaeecf86f19e7ebaa9c576740c86
hridayjham <jhamh@mcmaster.ca>
2021-01-22 14:48:49 -0500

    Made changes as the player

--- Lab1/code.py
+++ Lab1/code.py
@@ -4,6 +4,11 @@ def are_valid_groups(student_nos, groups):
     for student in student_nos:
         count = 0
         for group in groups:
+            count2=0
+            for x in group:
+                count2 += 1
+            if count2 > 3 or count2 < 2:
+                return False
             if student in group:
                 count += 1
             
@@ -12,7 +17,7 @@ def are_valid_groups(student_nos, groups):
 
     return True
 
-a = [1, 2, 3, 4, 5]
-b = [[1, 2], [3, 4], [5]]
+a = ["1", "2", "3", "4", "5", "6"]
+b = [["1", "2"], ["3", "4"], ["5"]]
 print(are_valid_groups(a, b))
                 
